0aa219e39c605d1497a378aaf1da7c2e13d13acf
joshdun31 <josh24dun@gmail.com>
2021-05-01 19:30:03 +0530

    inspect prevented

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,42 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+document.addEventListener('contextmenu',event=>{
+  event.preventDefault()
+})
+document.onkeydown = function(e) {
+  if(e.keyCode === 123) {
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'E'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode === 'I'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode === 'J'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'S'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'H'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'A'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'F'.charCodeAt(0)){
+  return false;
+  }
+  if(e.ctrlKey && e.keyCode === 'E'.charCodeAt(0)){
+  return false;
+  }
+  }
 ReactDOM.render(
   <React.StrictMode>
     <App />
